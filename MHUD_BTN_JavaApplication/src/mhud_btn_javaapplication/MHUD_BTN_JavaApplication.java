1f668c814c1047bf63c334d2f1fd630ebe3cadf8
hanquocadrian <hanquocadrian@gmail.com>
2020-04-28 08:59:41 +0700

    Idea charAt()

--- MHUD_BTN_JavaApplication/src/mhud_btn_javaapplication/MHUD_BTN_JavaApplication.java
+++ MHUD_BTN_JavaApplication/src/mhud_btn_javaapplication/MHUD_BTN_JavaApplication.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mhud_btn_javaapplication;
 
+import java.util.Scanner;
 import javax.swing.JFrame;
 
-/**
- *
- * @author Win10
- */
 public class MHUD_BTN_JavaApplication {
-
-    /**
-     * @param args the command line arguments
-     */
     
     public MHUD_BTN_JavaApplication() {
         JFrame frame = new JFrame("Hello Swing");
@@ -25,7 +13,17 @@ public class MHUD_BTN_JavaApplication {
     }
     
     public static void main(String[] args) {
-        new MHUD_BTN_JavaApplication();
+//        new MHUD_BTN_JavaApplication();
+        String a;
+        Scanner datain = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        a= datain.nextLine();
+        System.out.println(a);
+        char c;
+        for(int i=0;i< a.length();i++){
+            c= a.charAt(i);
+            System.out.println(c);
+        }
     }
     
 }
